a4a0a19627fb92e38661482d0c77bb6a40e8c727
Milena <milenahikari11@gmail.com>
2021-06-03 18:12:45 -0300

    Positioning card highlighted

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -14,13 +14,14 @@ export const Header = styled.View`
   background: ${({theme}) => theme.colors.primary};
 
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
   flex-direction: row;
 `;
 
 export const UserWrapper = styled.View`
   width: 100%;
   padding: 0 24px;
+  margin-top: ${RFValue(28)}px;
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
@@ -63,5 +64,7 @@ export const HighlightCards = styled.ScrollView.attrs({
   showsHorizontalScrollIndicator: false,
   contentContainerStyle: {paddingHorizontal: 24}
 })`
-  flex-direction: row;
+  width: 100%;
+  position: absolute;
+  margin-top: ${RFPercentage(20)}px;
 `;
