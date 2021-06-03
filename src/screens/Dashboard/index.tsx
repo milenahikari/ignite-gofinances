0b46e31d7c5e9d3aa7735f5749e60bd0fa749336
Milena <milenahikari11@gmail.com>
2021-06-03 15:50:50 -0300

    Create header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -5,7 +5,19 @@ import * as S from './styles';
 export function Dashboard() {
   return (
     <S.Container>
-      <S.Title>Dashboard</S.Title>
+      <S.Header>
+        <S.UserWrapper>
+          <S.UserInfo>
+            <S.Photo 
+              source={{uri: 'https://avatars.githubusercontent.com/u/45109055?v=4'}}
+            />
+            <S.User>
+              <S.UserGreeting>Olá,</S.UserGreeting>
+              <S.UserName>Milena</S.UserName>
+            </S.User>
+          </S.UserInfo>
+        </S.UserWrapper>
+      </S.Header>
     </S.Container>
   )
 }
