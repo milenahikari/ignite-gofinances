26e2fa4a761542da36918c359d118874793fe034
Milena <milena@wapstore.com.br>
2021-06-11 10:35:28 -0300

    Add style in categories list

--- App.tsx
+++ App.tsx
@@ -12,7 +12,7 @@ import {
 
 import theme from './src/global/styles/theme';
 // import {Dashboard} from './src/screens/Dashboard';
-import {Register} from './src/screens/Register';
+import {CategorySelect} from './src/screens/CategorySelect';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -31,7 +31,7 @@ export default function App() {
         barStyle="light-content"
         backgroundColor="#5636d3"
       />
-      <Register/>
+      <CategorySelect/>
     </ThemeProvider>
   );
 }
